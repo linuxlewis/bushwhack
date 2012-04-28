dbea84a3ec9d877fb5fb85a2c0ce115f3ddb8ad2
Sam Bolgert <sbolgert@gmail.com>
2012-04-28 11:42:03 -0500

    dumb

--- app/controllers/Game.js
+++ app/controllers/Game.js
@@ -1,13 +1,13 @@
 exports.mapRoutes = function(app){
-    app.get('/game/new', function(){
+    app.get('/game/new', function(req, res){
         res.render('/game/new.html');
     });
 
-    app.post('/game', function(){
+    app.post('/game', function(req, res){
 
     });
 
-    app.get('/game/:id', function(){
+    app.get('/game/:id', function(req, res){
 
     });
 }
