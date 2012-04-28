3be836eb9d80c935440f15a36db9745a46c7efc0
Sam Bolgert <sbolgert@gmail.com>
2012-04-28 12:04:20 -0500

    facebook login and demo

---
+++ app/controllers/Facebook.js
@@ -0,0 +1,4 @@
+exports.getMe = function(oauth_token){
+
+
+}
--- app/controllers/Game.js
+++ app/controllers/Game.js
@@ -1,6 +1,19 @@
+var request = require('request');
+
 exports.mapRoutes = function(app){
     app.get('/game/new', function(req, res){
-        res.render('game/new.html');
+        if(req.session.facebook.access_token){
+            request("https://graph.facebook.com/me?access_token="+req.session.facebook.access_token,
+                function(err, response, body){
+                    if(!err){
+                        var data = JSON.parse(body);
+                        res.render('game/new.html', {name:data.first_name});
+                    }
+                });
+        }
+        else{
+
+        }
     });
 
     app.post('/game', function(req, res){
