befaa057e07a39ca297edffeff706859b9690276
MikeSchuette <mike.schuette@gmail.com>
2012-04-28 20:49:19 -0500

    Update app/models/Course.js

--- app/models/Course.js
+++ app/models/Course.js
@@ -1,18 +1,31 @@
 var db = require('./model');
 var client = db.client;
 
+
 exports.findById = function(id, callback){
-   
    check(id).notEmpty().isInt();
    
-   client.query("SELECT * FROM courses WHERE id = " + id , function(err, result){
+   client.query("SELECT * FROM courses WHERE location_id = " + id , function(err, result){
        if(err){
             callback(err, null);
        }
        else{
            callback(null, result);
        }
+   });
+}
 
+
+exports.findByLocationId = function(id, callback){
+   check(id).notEmpty().isInt();
+   
+   client.query("SELECT * FROM courses WHERE location_id = " + id , function(err, result){
+       if(err){
+            callback(err, null);
+       }
+       else{
+           callback(null, result);
+       }
    });
 }
 
