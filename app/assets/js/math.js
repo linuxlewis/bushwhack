42e354a4aecd7d9311f42643e7e377ab431bfda4
Sam Bolgert <sbolgert@gmail.com>
2012-04-29 11:31:15 -0500

    images for tim

--- app/assets/js/math.js
+++ app/assets/js/math.js
@@ -9,5 +9,17 @@ function calculateDegree(lat1, lon1, lat2, lon2){
 }
 
 //console.log(calculateDegree(43.564472,-89.945068, 43.586359,-90.072784));
-console.log(calculateDegree(44.253069,-89.494629, 43.413029,-89.472656));
-console.log(calculateDegree(43.413029,-89.472656, 44.253069,-89.494629));
+//console.log(calculateDegree(44.253069,-89.494629, 43.413029,-89.472656));
+//console.log(calculateDegree(43.413029,-89.472656, 44.253069,-89.494629));
+
+
+//console.log(calculateDegree(43.413029,-89.472656, 44.111254,-88.59375));
+//console.log(calculateDegree(43.413029,-89.472656, 44.024422,-90.692139));
+
+//console.log(calculateDegree(44.111254,-88.59375,43.413029,-89.472656));
+
+console.log(calculateDegree(43.035034,-89.501778, 43.034942,-89.502696));
+var value = calculateDegree(43.035034,-89.501778, 43.034942,-89.502696);
+var rotation = 360 - (360 + 90 - value); 
+
+console.log(rotation);
