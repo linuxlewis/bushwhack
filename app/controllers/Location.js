ac7a505514d8e87ecfd7ed77447979afee86505e
Mike Schuette <mike.schuette@gmail.com>
2012-04-29 04:22:00 -0500

    Location.js

--- app/controllers/Location.js
+++ app/controllers/Location.js
@@ -1,21 +1,11 @@
+var util = require('util');
 var Location = require('../models/Location.js');
 var Course = require('../models/Course.js');
 
-/*
-var findByLocationId = (function(req, res){
-    Course.findByLocationId(req.params.id, function(err, result){
-        if(err){
-            res.json(err, 500)
-        }
-        else{
-            res.json(result);
-        }
-})(req, res);
-*/
 
 exports.mapRoutes = function(app){
-    app.get('/loc/near/:lat/:lng', function(req, res){
 
+    app.get('/loc/near/:lat/:lng', function(req, res){
         Location.findNearest(req.params.lat, req.params.lng, function(err, result){
             if(err){
                 res.json(err, 500);
@@ -38,7 +28,28 @@ exports.mapRoutes = function(app){
     });
     
     
-    //app.get('/loc/:id/courses', findByLocationId(req, res));
-    
+    app.get('/loc/:id/courses', function(req, res) {
+		Course.findByLocationId(req.params.id, function(err, result){
+			if(err){
+				res.json(err, 500)
+			}
+			else{
+				res.send('<p>result: </p>' + util.inspect(result));
+			}
+		});
+	});
+
+
+    app.get('/loc/:id/courses/:id/holes', function(req, res) {
+		//unfinished
+		Course.findByLocationId(req.params.id, function(err, result){
+			if(err){
+				res.json(err, 500)
+			}
+			else{
+				res.send('<p>result: </p>' + util.inspect(result));
+			}
+		});
+	});
 
 }
