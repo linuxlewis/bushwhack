22db76b1f88822cc59e398047c7d5325dc8fb00e
MikeSchuette <mike.schuette@gmail.com>
2012-04-29 01:01:28 -0500

    Update app/models/Tee.js

--- app/models/Tee.js
+++ app/models/Tee.js
@@ -1,1 +1,19 @@
-//stuff
+var db = require('./model');
+var client = db.client;
+
+var check = require('validator').check,
+    sanitize = require('validator').sanitize;
+
+
+exports.findById = function(id, callback){
+   check(id).notEmpty().isInt();
+   
+   client.query("SELECT * FROM tees WHERE id = " + id , function(err, result){
+       if(err){
+            callback(err, null);
+       }
+       else{
+           callback(null, result);
+       }
+   });
+}
