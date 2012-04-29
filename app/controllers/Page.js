0f1cb8c130ab96d8cae8cf07a3225b0fda234cdc
Sam Bolgert <sbolgert@gmail.com>
2012-04-29 12:27:35 -0500

    fb demo

--- app/controllers/Page.js
+++ app/controllers/Page.js
@@ -15,23 +15,22 @@ exports.mapRoutes = function(app){
     });
 
     app.get('/home', function(req, res){
-        res.render('mobile/home.html', {layout:'mobile_layout.html'});
-    });
-
-    app.get('/find', function(req, res){
         if(req.session.facebook){
             request("https://graph.facebook.com/me?access_token="+req.session.facebook.access_token,
                     function(err, response, body){
                         if(!err){
                             var data = JSON.parse(body);
-                            res.render('mobile/find.html', {layout:'mobile_layout.html', name:data.first_name});
+                            res.render('mobile/home.html', {layout:'mobile_layout.html', name:data.first_name});
                         }
                     });
         }
         else{
-            res.render('mobile/find.html', {layout:'mobile_layout.html'});
+            res.render('mobile/home.html', {layout:'mobile_layout.html'});
         }
+    });
 
+    app.get('/find', function(req, res){
+        res.render('mobile/find.html', {layout:'mobile_layout.html'});
     });
 
     app.get('/course_detail', function(req, res){
