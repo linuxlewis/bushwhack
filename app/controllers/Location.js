62a00ef8316e1a0c835fbce20a897942b16c5af5
MikeSchuette <mike.schuette@gmail.com>
2012-04-29 00:33:44 -0500

    Update app/controllers/Location.js

--- app/controllers/Location.js
+++ app/controllers/Location.js
@@ -3,7 +3,17 @@ var Course = require('../models/Course.js');
 
 
 exports.mapRoutes = function(app){
-    app.get('/location/near/:lat/:lng', function(req, res){
+    var findByLocationId = function(req, res){
+        Course.findByLocationId(req.params.id, function(err, result){
+            if(err){
+                res.json(err, 500)
+            }
+            else{
+                res.json(result);
+            }
+        });
+    
+    app.get('/loc/near/:lat/:lng', function(req, res){
 
         Location.findNearest(req.params.lat, req.params.lng, function(err, result){
             if(err){
@@ -15,7 +25,7 @@ exports.mapRoutes = function(app){
         });
     });
 
-    app.get('/location/:id', function(req, res){
+    app.get('/loc/:id', function(req, res){
         Location.findById(req.params.id, function(err, result){
             if(err){
                 res.json(err, 500)
@@ -27,14 +37,7 @@ exports.mapRoutes = function(app){
     });
     
     
-    app.get('/location/:id/courses', function(req, res){
-        Course.findByLocationId(req.params.id, function(err, result){
-            if(err){
-                res.json(err, 500)
-            }
-            else{
-                res.json(result);
-            }
-        });
-    });
+    app.get('/loc/:id/courses', findByLocationId);
+    
+
 }
