e28c534353eefda799815580dd1690ce129a2a96
Mike Schuette <mike.schuette@gmail.com>
2012-05-04 14:56:57 -0400

    attempt at bingery with rotation

--- app/assets/js/math.js
+++ app/assets/js/math.js
@@ -18,8 +18,27 @@ function calculateDegree(lat1, lon1, lat2, lon2){
 
 //console.log(calculateDegree(44.111254,-88.59375,43.413029,-89.472656));
 
+
+/*
 console.log(calculateDegree(43.035034,-89.501778, 43.034942,-89.502696));
 var value = calculateDegree(43.035034,-89.501778, 43.034942,-89.502696);
 var rotation = 360 - (360 + 90 - value) ; 
 
 console.log(rotation);
+*/
+
+
+function distance(lat1,lon1,lat2,lon2) {
+	var R = 6371; // km (change this constant to get miles)
+	var dLat = (lat2-lat1) * Math.PI / 180;
+	var dLon = (lon2-lon1) * Math.PI / 180; 
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) * 
+		Math.sin(dLon/2) * Math.sin(dLon/2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c;
+	if (d>1) return Math.round(d)+"km";
+	else if (d<=1) return Math.round(d*1000/1.1)+"yds";
+	return d;
+}
+
--- app/assets/js/ruler.js
+++ app/assets/js/ruler.js
@@ -69,18 +69,3 @@ var holePosition = new google.maps.LatLng(43.035034,-89.501778);
 	});
 
 }
-
-
-function distance(lat1,lon1,lat2,lon2) {
-	var R = 6371; // km (change this constant to get miles)
-	var dLat = (lat2-lat1) * Math.PI / 180;
-	var dLon = (lon2-lon1) * Math.PI / 180; 
-	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-		Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) * 
-		Math.sin(dLon/2) * Math.sin(dLon/2); 
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-	var d = R * c;
-	if (d>1) return Math.round(d)+"km";
-	else if (d<=1) return Math.round(d*1000/1.1)+"yds";
-	return d;
-}
